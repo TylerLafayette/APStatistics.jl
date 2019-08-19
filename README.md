ca1f7319ea3d1e0369986fea0530c5ff6feab1db
Tyler Lafayette <tylerlafayette@gmail.com>
2019-08-19 16:47:28 -0700

    Update README and add frequency table

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # APStatistics.jl
+
 Simple helper tools for AP Stats
+
+`Pkg.add("https://github.com/TylerLafayette/APStatistics.jl")`
--- src/APStatistics.jl
+++ src/APStatistics.jl
@@ -1,5 +1,13 @@
 module APStatistics
 
-greet() = print("Hello World!")
+function frequencytable(array::AbstractArray)
+    keys = unique(array)
+    frequencies = [[i, count(x->x == i, array)] for i in keys]
+    sort!(frequencies, by = x->x[2])
 
+    println("ITEM | COUNT")
+    for item in reverse(frequencies)
+        println("$(item[1]) | $(item[2])")
+    end
+end
 end # module
