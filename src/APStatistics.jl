e1441a0b3f380def54c228fd6553c86b6998d195
Tyler Lafayette <tylerlafayette@gmail.com>
2019-08-19 16:52:02 -0700

    Fix reversing

--- src/APStatistics.jl
+++ src/APStatistics.jl
@@ -3,13 +3,15 @@ module APStatistics
 function frequencytable(array::AbstractArray, orderbykeys = false)
     keys = unique(array)
     frequencies = [[i, count(x->x == i, array)] for i in keys]
-    sort!(frequencies, by = x->x[2])
     if orderbykeys
         sort!(frequencies, by = x->x[1])
+    else
+        sort!(frequencies, by = x->x[2])
+        reverse!(frequencies)
     end
 
     println("ITEM | COUNT")
-    for item in reverse(frequencies)
+    for item in frequencies
         println("$(item[1]) | $(item[2])")
     end
 end
