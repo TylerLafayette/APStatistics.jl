606a6a0f2bb19b3f688fef7fada3be844cd7cf8b
Tyler Lafayette <tylerlafayette@gmail.com>
2019-08-19 16:50:45 -0700

    Orderby for frequencytable

--- src/APStatistics.jl
+++ src/APStatistics.jl
@@ -1,9 +1,12 @@
 module APStatistics
 
-function frequencytable(array::AbstractArray)
+function frequencytable(array::AbstractArray, orderbykeys = false)
     keys = unique(array)
     frequencies = [[i, count(x->x == i, array)] for i in keys]
     sort!(frequencies, by = x->x[2])
+    if orderbykeys
+        sort!(frequencies, by = x->x[1])
+    end
 
     println("ITEM | COUNT")
     for item in reverse(frequencies)
